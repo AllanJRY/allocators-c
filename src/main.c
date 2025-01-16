437e4330e23aa4120a139c5160adfe5d61bb5058
ajarry <allan.jarry@mistral.fr>
2025-01-16 11:47:41 +0100

    feat(pool): add demo proc in main

--- src/main.c
+++ src/main.c
@@ -68,10 +68,40 @@ void demo_allocator_stack() {
     printf("\n\n");
 }
 
+void demo_allocator_pool() {
+    printf("# Pool Allocator\n\n");
+    uint8_t back_buf[sizeof(Position) * 10];
+
+    Allocator_Pool allocator;
+    allocator_pool_init(&allocator, back_buf, BACK_BUF_LEN, sizeof(Position), 32);
+
+    Position* pos_1 = (Position*) allocator_pool_alloc(&allocator);
+    pos_1->x = 10;
+    pos_1->y = 20;
+    printf("Position 1 (%08llx): x=%d y=%d\n", (uintptr_t)pos_1, pos_1->x, pos_1->y);
+
+    Position* pos_2 = (Position*) allocator_pool_alloc(&allocator);
+    pos_2->x = 90;
+    pos_2->y = 100;
+    printf("Position 2 (%08llx): x=%d y=%d\n", (uintptr_t)pos_2, pos_2->x, pos_2->y);
+
+    allocator_pool_free(&allocator, pos_2);
+    printf("Position 2 (%08llx) freed\n", (uintptr_t)pos_2);
+
+    // Should have same address as pos_2, because pos_2 has been freed.
+    Position* pos_3 = (Position*) allocator_pool_alloc(&allocator);
+    pos_3->x = 2;
+    pos_3->y = 56;
+    printf("Position 3 (%08llx): x=%d y=%d\n", (uintptr_t)pos_3, pos_3->x, pos_3->y);
+
+    allocator_pool_free_all(&allocator);
+    printf("\n\n");
+}
+
 int main(void) {
-
-    // demo_allocator_linear();
+    printf("\n");
+    demo_allocator_linear();
     demo_allocator_stack();
-
+    demo_allocator_pool();
     return 0;
 }
